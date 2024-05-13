9def04a026527a0a9205fae0fb32b3238a574fa9
UMMANCODE <ummanmm@code.edu.az>
2024-05-13 18:07:10 +0400

    Files added

---
+++ MvcPustok/MvcPustok/Controllers/BookController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MvcPustok.Data;
+using MvcPustok.Models;
+
+<<<<<<< HEAD
+namespace MvcPustok.Controllers {
+
+	public class BookController : Controller {
+		private readonly AppDbContext _context;
+		public BookController(AppDbContext context) {
+			_context = context;
+		}
+		public IActionResult GetBookById(int id) {
+			Book book = _context.Books.Include(x => x.Genre).Include(x => x.BookImages.Where(x => x.Status == true)).FirstOrDefault(x => x.Id == id);
+			return PartialView("_BookModalPartial", book);
+		}
+		public IActionResult Details(int id) {
+			Book? book = _context.Books.Include(x => x.BookImages).Include(x => x.Author)
+				.Include(x => x.Genre).Include(x => x.BookTags).ThenInclude(x => x.Tag).FirstOrDefault(x => x.Id == id);
+			if (book == null) {
+				return RedirectToAction("index", "home");
+			}
+			return View(book);
+		}
+	}
+}
+=======
+namespace MvcPustok.Controllers
+{
+    public class BookController : Controller
+    {
+        private readonly AppDbContext _context;
+        public BookController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult GetBookById(int id)
+        {
+            Book book = _context.Books.Include(x => x.Genre).Include(x => x.BookImages.Where(x => x.Status == true)).FirstOrDefault(x => x.Id == id);
+            return PartialView("_BookModalPartial", book);
+        }
+        public IActionResult Details(int id)
+        {
+            Book? book = _context.Books.Include(x => x.BookImages).Include(x => x.Author)
+                .Include(x => x.Genre).Include(x => x.BookTags).ThenInclude(x => x.Tag).FirstOrDefault(x => x.Id == id);
+            if (book == null)
+            {
+                return RedirectToAction("index", "home");
+            }
+            return View(book);
+        }
+    }
+}
+>>>>>>> da9b0fd76f04a55e7d764a76ca1fcd995f7c4a56
